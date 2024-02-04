98b960bc75268ab41c559cbbd9837a385204a41c
Wei Jie Yap <wyap@u90z02s03.42kl.edu.my>
2024-02-04 18:32:11 +0800

    task: fixed ft_unset code to work as intended

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,25 +1,20 @@
 #include "../../minishell.h"
 
 /*
-* check key name against key in env->env_var
-* returns an updated list
-* ! NOT WORKING IF ALL VARIABLE NOT EXIST IN env_var
+* check if any variable in del matches that in current env_var
+* returns count of matches;
 */
-int	check_del(t_env *env, char **del)
+int	get_valid_count(t_env *env, char **del)
 {
-	char	*tmp;
 	int		i;
 	int		j;
 	int		count;
+	char	*tmp;
 
 	i = 0;
 	count = 0;
 	while (del[i])
 	{
-		if (!ft_strcmp(del[i], "_"))
-			i++;
-		if (ft_strchr(del[i], '='))
-			i++;
 		j = 0;
 		while (env->env_vars[j])
 		{
@@ -28,9 +23,7 @@ int	check_del(t_env *env, char **del)
 				tmp = env->env_vars[j];
 				env->env_vars[j] = ft_strdup(" ");
 				free(tmp);
-				// env->env_vars[j] = " ";
 				count++;
-				// j++;
 			}
 			// printf("check del mark\ni:%d\n", i);
 			j++;
@@ -39,6 +32,73 @@ int	check_del(t_env *env, char **del)
 	}
 	return (count);
 }
+
+/*
+* filter invalid arguments cases:
+* variable "_" cannot be unset; variable ending with = is invalid
+* other invalid arguments are not counted when get_count
+* updates list of variables to unset
+*/
+void	check_del(char **del)
+{
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	while (del[i])
+	{
+		if (ft_strchr(del[i], '=') || !ft_strcmp(del[i], "_"))
+		{
+			printf("replacing %s to blank\n", del[i]);
+			tmp = del[i];
+			del[i] = ft_strdup(" ");
+			free(tmp);
+			if (del[i + 1])
+				i++;
+		}
+		else
+			i++;
+	}
+}
+
+// int	check_del(t_env *env, char **del)
+// {
+// 	char	*tmp;
+// 	int		i;
+// 	int		j;
+// 	int		count;
+
+// 	i = 0;
+// 	count = 0;
+// 	while (del[i])
+// 	{
+// 		if (!ft_strcmp(del[i], "_") && del[i + 1])
+// 			i++;
+// 		if (ft_strchr(del[i], '='))
+// 		{
+// 			tmp = del[i];
+// 			del[i] = ft_strdup(" ");
+// 			free(tmp);
+// 			if (del[i + 1])
+// 			i++;
+// 		}
+// 		j = 0;
+// 		while (env->env_vars[j])
+// 		{
+// 			if (del[i] && !ft_strncmp(del[i], env->env_vars[j], ft_strlen(del[i])))
+// 			{
+// 				tmp = env->env_vars[j];
+// 				env->env_vars[j] = ft_strdup(" ");
+// 				free(tmp);
+// 				count++;
+// 			}
+// 			// printf("check del mark\ni:%d\n", i);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	return (count);
+// }
 
 char	**copy_unset_env(t_env *env, int count)
 {
@@ -50,7 +110,7 @@ char	**copy_unset_env(t_env *env, int count)
 	j = 0;
 	res = (char **)malloc((count + 1) * sizeof(char *)); //+1 ?
 	if (!res)
-		printf("export: copy_env() malloc failed");
+		printf("unset: copy_env() malloc failed");
 	while (env->env_vars[i] && i < env->key_count)
 	{
 		if (!ft_strcmp(env->env_vars[i], " ")) //skip if match
@@ -75,68 +135,48 @@ char	**copy_unset_env(t_env *env, int count)
 
 void	ft_unset(t_env *env, char **cmds)
 {
-	char	**del_vars;
 	char	**res;
 	int		count;
 
 	count = 0;
-	del_vars = cmds;
-	for (int k = 0; del_vars[k]; k++)
-		printf("%d: %s\n", k, del_vars[k]);
-
 	printf("ft_unset: current env key count: %d\n", env->key_count);
-	count = check_del(env, del_vars);
+	for (int k = 0; cmds[k]; k++)
+		printf("%d: %s\n", k, cmds[k]);
+	check_del(cmds);
+	for (int k = 0; cmds[k]; k++)
+		printf("%d: %s\n", k, cmds[k]);
+	count = get_valid_count(env, cmds);
 	printf("unset count:%d\n", count);
-	// free_2d_arr(del_vars);
-	// print_sys_env(env);
 	if (!count)
 		return ;
 	res = copy_unset_env(env, env->key_count - count);
 	printf("ft_unset: updated env key count: %d\n", env->key_count);
-	// res[env->key_count] = NULL;
-	// print_sys_env(env);
-	// free_2d_arr(env->env_vars);
-	// for (int k = 0; k <= env->key_count; k++)
-	// 	printf("%d: %s\n", k, res[k]);
-
-		// free the unchanged env->env_vars (env->key_count - count) that will replace by res**
-		// int i = 0;
-		// while (env->env_vars[i] && i < env->key_count - count + 1)
-		// {
-		// 	free(env->env_vars[i]);
-		// 	i++;
-		// }
-
-	// env->env_vars = (char **)malloc((env->key_count) * sizeof(char *));
 	free_2d_arr(env->env_vars);
 	env->env_vars = res;
 }
 
-// void	ft_unset(t_env *env, char *del)
+// void	ft_unset(t_env *env, char **cmds)
 // {
 // 	char	**del_vars;
 // 	char	**res;
 // 	int		count;
 
 // 	count = 0;
-// 	del_vars = ft_split(del, ' ');
+// 	del_vars = cmds;
 // 	for (int k = 0; del_vars[k]; k++)
 // 		printf("%d: %s\n", k, del_vars[k]);
 
 // 	printf("ft_unset: current env key count: %d\n", env->key_count);
 // 	count = check_del(env, del_vars);
+// 	for (int k = 0; del_vars[k]; k++)
+// 		printf("%d: %s\n", k, del_vars[k]);
 // 	printf("unset count:%d\n", count);
-// 	free_2d_arr(del_vars);
-// 	// print_sys_env(env);
 // 	if (!count)
 // 		return ;
 // 	res = copy_unset_env(env, env->key_count - count);
 // 	printf("ft_unset: updated env key count: %d\n", env->key_count);
-// 	// res[env->key_count] = NULL;
-// 	// print_sys_env(env);
-// 	// free_2d_arr(env->env_vars);
-// 	for (int k = 0; k <= env->key_count; k++)
-// 		printf("%d: %s\n", k, res[k]);
+// 	// for (int k = 0; k <= env->key_count; k++)
+// 	// 	printf("%d: %s\n", k, res[k]);
 
 // 		// free the unchanged env->env_vars (env->key_count - count) that will replace by res**
 // 		// int i = 0;
@@ -145,8 +185,6 @@ void	ft_unset(t_env *env, char **cmds)
 // 		// 	free(env->env_vars[i]);
 // 		// 	i++;
 // 		// }
-
-// 	// env->env_vars = (char **)malloc((env->key_count) * sizeof(char *));
 // 	free_2d_arr(env->env_vars);
 // 	env->env_vars = res;
 // }
